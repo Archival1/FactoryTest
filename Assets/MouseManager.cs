3353aa52e112c865a605211c957e53e60fddd4d8
Archival <ganoosh2@gmail.com>
2024-05-18 17:30:18 +1000

    ADA

--- Assets/MouseManager.cs
+++ Assets/MouseManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MouseManager : MonoBehaviour
 {
@@ -8,6 +10,19 @@ public class MouseManager : MonoBehaviour
     [SerializeField] private LayerMask _placementLayerMask;
 
     private Vector3 _lastPosition;
+
+    public event Action OnClick, OnExit;
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            OnClick?.Invoke();
+        }
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            OnExit?.Invoke();
+        }
+    }
+
+    public bool IsPointerOverUi() => EventSystem.current.IsPointerOverGameObject();
 
     public Vector3 GetSelectedGridPos() { 
         Vector3 mousePos = Input.mousePosition;
--- Assets/PlacementSystem.cs
+++ Assets/PlacementSystem.cs
@@ -8,6 +8,47 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField] private MouseManager mouseManager;
     [SerializeField] private Grid grid;
 
+    [SerializeField] private ObjectManager objectManager;
+    private int selectedObjectIndex = 0;
+
+    [SerializeField] private GameObject gridVis;
+
+    private void Start() {
+        StopPlacement();
+    }
+
+    public void StopPlacement() {
+        Debug.Log("StopPlacement");
+        selectedObjectIndex = 0;
+        gridVis.SetActive(false);
+        mouseManager.OnClick -= PlaceStructure;
+        mouseManager.OnExit -= StopPlacement;
+    }
+
+    public void StartPlacement(int id) {
+        Debug.Log("StartPlacement");
+        selectedObjectIndex = objectManager.objectsData.FindIndex(d => d.Id == id);
+        if (selectedObjectIndex < 0) {
+            Debug.LogError($"No object for {id} found");
+            return;
+        } else {
+            gridVis.SetActive(true);
+            mouseManager.OnClick += PlaceStructure;
+            mouseManager.OnExit += StopPlacement;
+        }
+    }
+
+    public void PlaceStructure() {
+        if (mouseManager.IsPointerOverUi()) {
+            return;
+        }
+
+        Vector3 mousePosition = mouseManager.GetSelectedGridPos();
+        Vector3Int gridPos = grid.WorldToCell(mousePosition);
+        GameObject gameObject = Instantiate(objectManager.objectsData[selectedObjectIndex].Prefab);
+        gameObject.transform.position = grid.CellToWorld(gridPos);
+    }
+
     private void Update() {
         Vector3 mousePos = mouseManager.GetSelectedGridPos();
         Vector3Int gridPos = grid.WorldToCell(mousePos);
